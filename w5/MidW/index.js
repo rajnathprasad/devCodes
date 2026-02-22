97f9fb4dd5a707e410c075a9cd0d4e90a6980ffd
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-02-22 23:58:00 +0530

    w5

--- w5/MidW/index.js
+++ w5/MidW/index.js
@@ -6,18 +6,50 @@ let requestCount=0;
 function requestIncreaser(req,res,next){
     requestCount+=1;
     console.log(`Total number of requests : ${requestCount}`);
+    console.log(`URL : ${req.url}`)
+    console.log(`METHOD : ${req.method}`)
+    console.log(`Timestamp : ${Date.now()}`)
+
     next();
 }
 
-function realSumHandler(req,res){
+function adder(req,res){
     res.json({
         result :
         parseInt(req.query.a)+parseInt(req.query.b)
     })
 }
+function subtractor(req,res){
+    res.json({
+        result :
+        parseInt(req.query.a)-parseInt(req.query.b)
+    })
+}
+function divisor(req,res){
+    res.json({
+        result :
+        parseInt(req.query.a)/parseInt(req.query.b)
+    })
+}
+function multiplier(req,res){
+    res.json({
+        result :
+        parseInt(req.query.a)*parseInt(req.query.b)
+    })
+}
 
+app.get("/admin",function(req,res){
+    res.json({
+        "Total number of requests" : requestCount
+    })
+})
 
-app.get("/sum",requestIncreaser,realSumHandler);
+app.use(requestIncreaser)
+
+app.get("/add",adder);
+app.get("/subtract",subtractor);
+app.get("/divide",divisor);
+app.get("/multiply",multiplier);
 
 
 app.listen(3000)
