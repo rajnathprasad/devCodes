4ad6a87314074387ec65d1bdb8cc83685395d8ed
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-03-04 12:09:59 +0530

    post component

--- impl9/src/App.jsx
+++ impl9/src/App.jsx
@@ -1,8 +1,28 @@
 function App() {
   return (
     <div>
-      Hi there
+      <PostComponent/>
     </div>
+  )
+}
+
+const style = {width : 200, backgroundColor : "white", border : "1px solid gray", display : "flex"}
+
+function PostComponent(){
+  return (
+  <div style={style}>
+    <img src={"https://cdn.pixabay.com/photo/2025/05/24/12/40/whale-9619752_640.png"} style={
+      {width:40,
+      height:40,
+      borderRadius :"50%"}
+    }/>
+    <div>
+      <b>Dolphin</b>
+      <div>1000 followers</div>
+      <div>1 minute ago</div>
+      <div>You guys don't even know my pain.. I am tired of swimming.. I want to walk now</div>
+    </div>
+  </div>
   )
 }
 
