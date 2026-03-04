c908a6ce3cd10a404489a35700b74841b2bd5e23
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-03-04 15:35:36 +0530

    three comps

--- impl9/src/App.jsx
+++ impl9/src/App.jsx
@@ -1,29 +1,49 @@
 function App() {
   return (
-    <div>
-      <PostComponent/>
+    <div style={{ background: "#ecf0f1", height: "100vh"}}>
+      <div style = {{fontFamily: "system-ui", display: "flex", justifyContent: "center" }}>
+        <div>
+        <div style={{padding : 2}}><PostComponent /></div>
+        <div style={{padding : 2}}><PostComponent /></div>
+        <div style={{padding : 2}}><PostComponent /></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-const style = {width : 200, backgroundColor : "white", border : "1px solid gray", display : "flex"}
+const style = {
+  width: 300,
+  backgroundColor: "white",
+  border: "1px solid gray",
+  borderRadius: 10,
+  height : "100%"
+};
 
-function PostComponent(){
+function PostComponent() {
   return (
-  <div style={style}>
-    <img src={"https://cdn.pixabay.com/photo/2025/05/24/12/40/whale-9619752_640.png"} style={
-      {width:40,
-      height:40,
-      borderRadius :"50%"}
-    }/>
-    <div>
-      <b>Dolphin</b>
-      <div>1000 followers</div>
-      <div>1 minute ago</div>
-      <div>You guys don't even know my pain.. I am tired of swimming.. I want to walk now</div>
+    <div style={style}>
+      <div style={{ display: "flex", margin: "10px" }}>
+        <img
+          src={
+            "https://cdn.pixabay.com/photo/2025/05/24/12/40/whale-9619752_640.png"
+          }
+          style={{ width: 40, height: 40, borderRadius: "50%" }}
+        />
+        <div style={{ fontSize: 12, marginLeft: 8 }}>
+          <b>Dolphin</b>
+          <div style={{ color: "#7f8c8d", fontSize: 9, lineHeight:1}}>
+            <div>1000 followers</div>
+            <div>1 minute ago</div>
+          </div>
+        </div>
+      </div>
+      <div style={{ fontSize: "13px" , margin : 7}}>
+        You guys don't even know my pain.. I am tired of swimming.. I want to
+        walk now
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default App
+export default App;
