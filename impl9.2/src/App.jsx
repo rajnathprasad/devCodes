74108cb2da391c4c64e20e9cf3bc1d66aad5f75d
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-03-05 12:37:14 +0530

    tabswitcher

--- impl9.2/src/App.jsx
+++ impl9.2/src/App.jsx
@@ -1,17 +1,23 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
 
 function App() {
+  
+
   const [count, setCount] = useState(0)
   function increaseCount(){
-    setCount(count+1)
+    setCount(c=>c+1)
   }
+  useEffect(function(){
+    setInterval(increaseCount,1000)
+  },[])
 
   return (<div>
     {count>0 && <div style={{background : "red", width : 25, height : 25, textAlign:"center", borderRadius : "50%", color : "white", position : "absolute", marginLeft : "50px", marginTop : "10px", display:"flex", alignItems:"center", justifyContent:"center"}}>
       {(count>9)?"9+":count}
     </div>}
     <img src="https://icones.pro/wp-content/uploads/2022/02/icone-de-cloche-grise.png" alt="" style={{width:80, cursor :"pointer"}} />
-    <button onClick={increaseCount}>Send Notification</button>
+    {/* <button onClick={increaseCount}>Send Notification</button> */}
   </div>)
 }
 
---
+++ impl9.3/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [currentTab, setCurrentTab] = useState(1);
+  const [tabData, setTabData] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(function(){
+    setLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/todos/"+currentTab)
+    .then(async res=>{
+      const json = await res.json();
+      setTabData(json);
+      setLoading(false);
+    })
+  }, [currentTab])
+
+  return (<div>
+    <button onClick={function(){
+      setCurrentTab(1)
+    }} style={{color : currentTab==1?"red":"black"}}>Todo 1</button>
+    <button onClick={function(){
+      setCurrentTab(2)
+    }} style={{color : currentTab==2?"red":"black"}}>Todo 2</button>
+    <button onClick={function(){
+      setCurrentTab(3)
+    }} style={{color : currentTab==3?"red":"black"}}>Todo 3</button>
+    <button onClick={function(){
+      setCurrentTab(4)
+    }} style={{color : currentTab==4?"red":"black"}}>Todo 4</button>
+    <br />
+    {loading?"loading...":tabData.title}
+  </div>
+  )
+}
+
+export default App
