dbc8b212fe0a25064213c651ef5a67e9c69db521
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-03-06 12:32:35 +0530

    outlet layout

--- impl10/src/App.jsx
+++ impl10/src/App.jsx
@@ -1,51 +1,78 @@
-import {BrowserRouter, Routes, Route, Link, useNavigate} from "react-router-dom"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  Outlet
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div>
       <BrowserRouter>
-      <div>
-      <Link to="/">Homepage </Link>
-      |
-      <Link to="/java">JAVA </Link> 
-      |
-      <Link to="/cpp">CPP </Link> 
-      </div>
-      <Routes>
-        <Route path="/" element={<Landing/>}/>
-        <Route path="/java" element={<Java/>}/>
-        <Route path="/cpp" element={<Cpp/>}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Landing />} />
+            <Route path="/java" element={<Java />} />
+            <Route path="/cpp" element={<Cpp />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
-    </>
-  )
+    </div>
+  );
 }
 
-function Landing(){
-  return(
-    <>
-    Welcome to the Landing Page
-    </>
-  )
+function Layout() {
+  return (
+    <div>
+      <header style={{textAlign : "center"}}>
+        <Link to="/">Homepage </Link>|<Link to="/java">JAVA </Link>|
+        <Link to="/cpp">CPP </Link>
+      </header>
+      <div style={{height : "80vh", display : "flex", justifyContent: "center", alignItems : "center"}}>
+      <Outlet />
+      </div>
+      <footer style={{textAlign : "center"}}>FOOTER</footer>
+      
+    </div>
+  );
 }
-function Java(){
-  return(
-    <>
-    Welcome to the Java Course
-    </>
-  )
+
+function Landing() {
+  return <div>Welcome to the Landing Page</div>;
 }
-function Cpp(){
+function Java() {
+  return <div>Welcome to the Java Course</div>;
+}
+function Cpp() {
   const navigate = useNavigate();
-  function redirectUser(){
-    navigate("/")
+  function redirectUser() {
+    navigate("/");
   }
-  return(
-    <>
-    Welcome to the CPP Course
-    <button onClick={redirectUser}>Go to landing page</button>
-    </>
-  )
+  return (
+    <div>
+      Welcome to the CPP Course
+      <div>
+      <button onClick={redirectUser}>Go to landing page</button>
+      </div>
+    </div>
+  );
+}
+function ErrorPage() {
+  const navigate = useNavigate();
+  function redirectUser() {
+    navigate("/");
+  }
+  return (
+    <div>
+      Sorry, Page not found
+      <div>
+      <button onClick={redirectUser}>Go to landing page</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
