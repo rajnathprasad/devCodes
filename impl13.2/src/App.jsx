79e29ba9245117d434349ae57ad7c7092f86d844
rajnathprasad <rajnathprasad.cs@gmail.com>
2026-04-06 22:36:39 +0530

    issue

--- impl13.2/src/App.jsx
+++ impl13.2/src/App.jsx
@@ -8,10 +8,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='flex flex-col bg-green-500 sm:flex-row-bg-blue-500 sm:flex-row'>
-      <div className='bg-green-500 sm:bg-blue-500'>Child 1</div>
-      <div className='bg-green-500 sm:bg-blue-500'>Child 2</div>
-      <div className='bg-green-500 sm:bg-blue-500'>Child 3</div>
+    <div className='flex'>
+      
     </div>
   )
 }
